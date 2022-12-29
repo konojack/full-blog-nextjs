9cd43228ddfa9b5e3b089e41c8e5feb277496eda
konojack <konojack@gmail.com>
2022-12-29 13:37:48 +0100

    added navigation part

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import Layout from '../components/home-page/layout/layout';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
