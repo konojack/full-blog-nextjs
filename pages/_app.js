50b62067f263b76b8841619a2bb1ba3966a8d414
konojack <konojack@gmail.com>
2022-12-29 15:46:06 +0100

    change Layout dir

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from '../components/home-page/layout/layout';
+import Layout from '../components/layout/layout';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
