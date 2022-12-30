86fb3f576ccbe7ed6bbd6fd9799267915da2be8e
konojack <konojack@gmail.com>
2022-12-30 12:46:16 +0100

    created posts components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
 import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
 
+const DUMMY_POSTS = [
+  {
+    slug: 'slug',
+    title: 'title',
+    image: 'example-image.png',
+    excerpt: 'Blablabla test test',
+    date: '2022-12-31',
+  },
+  {
+    slug: 'slug',
+    title: 'title',
+    image: 'example-image.png',
+    excerpt: 'Blablabla test test',
+    date: '2022-12-31',
+  },
+  {
+    slug: 'slug',
+    title: 'title',
+    image: 'example-image.png',
+    excerpt: 'Blablabla test test',
+    date: '2022-12-31',
+  },
+  {
+    slug: 'slug',
+    title: 'title',
+    image: 'example-image.png',
+    excerpt: 'Blablabla test test',
+    date: '2022-12-31',
+  },
+];
 export default function HomePage() {
   return (
-    <div>
+    <>
       <Hero />
-      <FeaturedPosts />
-    </div>
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </>
   );
 }
