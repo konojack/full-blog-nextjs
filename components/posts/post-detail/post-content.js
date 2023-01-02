fdb60f59da98e400f3994ba72efaa4fe75756ad0
konojack <konojack@gmail.com>
2023-01-02 18:25:36 +0100

    read image inside MD files

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,13 +1,29 @@
+import Image from 'next/image';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
 
 const PostContent = ({ postData }) => {
   const imagePath = `/images/posts/${postData.slug}/${postData.image}`;
+
+  const customRenderers = {
+    img(image) {
+      return (
+        <Image
+          src={`/images/posts/${postData.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
   return (
     <article class={classes.content}>
       <PostHeader image={imagePath} title={postData.title} />
-      <ReactMarkdown>{postData.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>
+        {postData.content}
+      </ReactMarkdown>
     </article>
   );
 };
